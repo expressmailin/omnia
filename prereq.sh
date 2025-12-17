f5542fcd1c4dcfb4ec7192fed93b4f2705dcff68
expressmailin <66966822+expressmailin@users.noreply.github.com>
2025-12-17 21:02:01 +0530

    Update prereq.sh
    Signed-off-by: expressmailin <66966822+expressmailin@users.noreply.github.com>

--- prereq.sh
+++ prereq.sh
@@ -2,7 +2,7 @@
 
 
 # Copyright © 2024 Dell Inc. or its subsidiaries. All Rights Reserved.
-# New
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
